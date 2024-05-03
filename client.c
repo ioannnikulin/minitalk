26c6b52d159f514bf04e4e6d718b49303ebd9af4
ioann <ioann.nikulin@gmail.com>
2024-05-03 18:13:11 +0200

    started bonus

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: inikulin <inikulin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 18:42:42 by inikulin          #+#    #+#             */
-/*   Updated: 2024/05/01 21:44:39 by inikulin         ###   ########.fr       */
+/*   Updated: 2024/05/02 21:04:09 by inikulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,17 +32,35 @@ static void	send_number(int num, int pid)
 	}
 }
 
+static int	sym_len(char *s)
+{
+	unsigned char	c;
+
+	c = *s;
+	if (c & 128 == 0)
+		return (1);
+	if (c & 192 == 192)
+		return (2);
+	if (c & 224 == 224)
+		return (3);
+	if (c & 240 == 240)
+		return (4);
+	if (c & 248 == 248)
+		return (5);
+	return (6);
+}
+
 static void	send_text(char *s, int pid)
 {
 	int				sym_i;
 	unsigned char	bit_mask;
+	int				sym_len;
 
-	sym_i = -1;
-	while (s[++ sym_i])
+	sym_i = 0;
+	while (s[sym_i])
 	{
-		kill(pid, SIGUSR1);
-		usleep(100);
-		bit_mask = 1 << 6;
+		sym_len = sym_len(s[sym_i]);
+		bit_mask = 1 << (8 * sym_len - 1);
 		while (bit_mask)
 		{
 			if (s[sym_i] & bit_mask)
@@ -52,6 +70,7 @@ static void	send_text(char *s, int pid)
 			usleep(100);
 			bit_mask >>= 1;
 		}
+		sym_i += sym_len;
 	}
 }
 
